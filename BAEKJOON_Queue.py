79a20988a88d932417e0f8dbf568671896912d09
신정섭 <shinjeongsub@sinjeongseob-ui-MacBookPro.local>
2023-01-23 22:10:14 +0900

    11866번

--- BAEKJOON_Queue.py
+++ BAEKJOON_Queue.py
@@ -56,3 +56,57 @@ for i in range(n):
 
     elif str[0] == "back":
         print(back())
+
+
+# 2164번
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+card = []
+front_index = 0
+
+for i in range(n):
+    card.append(i+1)
+
+if len(card) == 1:
+    print(card[0])
+
+
+else:
+    while True:
+        front_index += 2
+        card.append(card[front_index-1])
+        if len(card) - front_index == 1:
+            break
+
+    print(card[front_index])
+
+
+# 11866번
+import sys
+input = sys.stdin.readline
+
+n, k = input().split()
+n = int(n)
+k = int(k)
+
+queue = []
+front_index = 0 # queue의 front_index
+permutation = [] # 순열
+
+for i in range(1, n+1):
+    queue.append(i)
+
+while len(permutation) != n:
+    for i in range(k-1):
+        queue.append(queue[front_index])
+        front_index += 1
+    permutation.append(queue[front_index])
+    front_index += 1
+
+print("<", end="")
+for i in permutation:
+    if i != permutation[-1]:
+        print(f"{i}, ", end="")
+    else: print(f"{i}>")
