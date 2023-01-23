2d93e6333fb331405ed66d27e7f8e3ec9e54262c
신정섭 <shinjeongsub@sinjeongseob-ui-MacBookPro.local>
2023-01-23 19:13:18 +0900

    18258번

---
+++ BAEKJOON_Queue.py
@@ -0,0 +1,58 @@
+# 18258번
+import sys
+input = sys.stdin.readline
+
+queue = []
+front_index = 0
+
+n = int(input())
+
+def push(x):
+    queue.append(x)
+
+def pop():
+    global front_index # 함수 내에서 값을 수정하기 위해선 전역변수로 선언!
+    if len(queue) != front_index:
+        x = queue[front_index]
+        front_index += 1
+        return x
+    else: return -1
+
+def size():
+    return len(queue) - front_index
+
+def empty():
+    if len(queue) == front_index:
+        return 1
+    else: return 0
+
+def front():
+    if len(queue) != front_index:
+        return queue[front_index]
+    else: return -1
+
+def back():
+    if len(queue) != front_index:
+        return queue[-1]
+    else: return -1
+
+for i in range(n):
+    str = input().split()
+
+    if len(str) == 2:
+        push(str[1])
+    
+    elif str[0] == "pop":
+        print(pop())
+    
+    elif str[0] == "size":
+        print(size())
+    
+    elif str[0] == "empty":
+        print(empty())
+    
+    elif str[0] == "front":
+        print(front())
+
+    elif str[0] == "back":
+        print(back())
