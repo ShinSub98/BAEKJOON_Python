2e5dc65dc0ce79ad67a26d2230c3555050b180fc
신정섭 <shinjeongsub@sinjeongseob-ui-MacBookPro.local>
2023-01-25 00:05:02 +0900

    String

--- BAEKJOON_String.py
+++ BAEKJOON_String.py
@@ -6,3 +6,32 @@ if dType == str:
 	print(ord(a))
 else: print(chr(a))
 
+
+# 11720번
+n = int(input())
+
+x = input()
+sum = 0
+for i in x:
+	sum += int(i)
+
+print(sum)
+
+
+# 10809번
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+s = input()
+
+for i in alphabet:
+	check = False
+
+	for j in range(len(s)):
+		if s[j] == i:
+			check = True
+			x = j
+			break
+	
+	if check == True:
+		print(x, end=" ")
+	else: print(-1, end= " ")
