e9995fedb3e264dfaf38f966379ff09184c0871a
신정섭 <shinjeongsub@sinjeongseob-ui-MacBookPro.local>
2023-01-25 18:03:56 +0900

    확인

--- BAEKJOON_BASIC_MATH.py
+++ BAEKJOON_BASIC_MATH.py
@@ -12,3 +12,20 @@ if b>=c:
 else:
 	sale = a//s
 	print(sale+1)
+
+
+# 2292번
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+
+x=1
+count = 1
+
+while a>x:
+	x += count*6
+	count +=1
+
+print(count)
+
