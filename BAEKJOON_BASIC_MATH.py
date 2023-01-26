2adb64cc764bd62c05cba80e98b58f2272469db3
신정섭 <shinjeongsub@sinjeongseob-ui-MacBookPro.local>
2023-01-26 18:40:21 +0900

    1193번

--- BAEKJOON_BASIC_MATH.py
+++ BAEKJOON_BASIC_MATH.py
@@ -29,3 +29,25 @@ while a>x:
 
 print(count)
 
+
+# 1193번
+import sys
+input = sys.stdin.readline().rstrip
+
+x = int(input()) # 순서
+
+n = 1
+sum = 1
+
+while sum < x:
+	n += 1
+	sum += n
+
+sum -= n	
+x -= sum
+a = n+1-x
+
+if n%2==1: # 홀수줄이면
+	print(f"{a}/{x}")
+else:
+	print(f"{x}/{a}")
