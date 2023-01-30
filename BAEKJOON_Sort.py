3f115050471b0cd56f4f88974293b5ffbfafdee5
신정섭 <wjdtjq26@naver.com>
2023-01-31 03:18:32 +0900

    추가

--- BAEKJOON_Sort.py
+++ BAEKJOON_Sort.py
@@ -65,3 +65,94 @@ for i in range(len(count)):
     if count[i] != 0:
         for j in range(count[i]):
             print(i)
+
+
+# 2108번
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+count = [0] * 8001
+
+for i in range(n):
+    count[int(input()) + 4000] += 1
+
+if count.count(max(count)) >= 2:
+    firstIdx = count.index(max(count))
+    idx = count.index(max(count), firstIdx+1)
+else: idx = count.index(max(count))
+
+mode = idx-4000 # 최빈값
+
+nums = []
+
+for i in range(len(count)):
+    if count[i] != 0:
+        for j in range(count[i]):
+            nums.append(i-4000)
+
+print(round(sum(nums)/len(nums)))
+print(nums[int(n/2)])
+print(mode)
+print(max(nums) - min(nums))
+
+
+# 1427번
+lst = list(map(int, input()))
+
+lst.sort(reverse=True)
+
+for i in lst:
+    print(i, end="")
+
+
+# 11650번
+n = int(input())
+
+nums = []
+
+for i in range(n):
+    nums.append(list(map(int, input().split())))
+
+nums.sort(key=lambda x:x[1], reverse=False)
+nums.sort(key=lambda x:x[0], reverse=False)
+
+for i in nums:
+    print(i[0], i[1])
+
+
+# 1181번
+n = int(input())
+
+nums = []
+
+for i in range(n):
+    s = input()
+    nums.append([s, len(s)]) # [단어, 길이]
+
+nums.sort(key=lambda x:x[0], reverse=False)
+nums.sort(key=lambda x:x[1], reverse=False)
+
+for i in range(len(nums)-1, 0, -1):
+    if nums[i] == nums[i-1]:
+        del nums[i]
+
+for i in nums:
+    print(i[0])
+
+
+# 10814번
+n = int(input())
+
+people = []
+
+for i in range(n):
+    info = list(map(str, input().split()))
+    info[0] = int(info[0])
+    people.append(info)
+
+people.sort(key=lambda x:x [0], reverse = False)
+
+for i in people:
+    print(i[0], i[1])
