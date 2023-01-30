ca2bcf4d1a819c72ebc4596f15f36b1db8e9b2d4
신정섭 <wjdtjq26@naver.com>
2023-01-30 23:23:24 +0900

    추가

--- BAEKJOON_Sort.py
+++ BAEKJOON_Sort.py
@@ -48,3 +48,20 @@ lst.sort()
 
 for i in lst:
     print(i)
+
+
+# 10989번
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+count = [0] * 10001
+
+for i in range(n):
+    count[int(input())] += 1
+
+for i in range(len(count)):
+    if count[i] != 0:
+        for j in range(count[i]):
+            print(i)
