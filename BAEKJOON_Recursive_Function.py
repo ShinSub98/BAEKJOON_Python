359b06c5fa672619742d519f865ea6bde61280f4
신정섭 <wjdtjq26@naver.com>
2023-02-07 16:03:38 +0900

    추가

--- BAEKJOON_Recursive_Function.py
+++ BAEKJOON_Recursive_Function.py
@@ -7,3 +7,32 @@ def fac(n):
 
 n = int(input())
 print(fac(n))
+
+# 10870번
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+n = int(input())
+print(fibo(n))
+
+
+# 25501번
+def recursion(s, l, r, count):
+    if l >= r: return f"1 {count}"
+    elif s[l] != s[r]: return f"0 {count}"
+    else: return recursion(s, l+1, r-1, count+1)
+
+def isPalindrome(s):
+    count = 1
+    return recursion(s, 0, len(s)-1, count)
+
+t = int(input())
+
+for i in range(t):
+    s = input()
+    print(isPalindrome(s))
