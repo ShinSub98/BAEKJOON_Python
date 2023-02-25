363bd4bf3d9699dc920c216e21e9c61f64d48baf
신정섭 <wjdtjq26@naver.com>
2023-02-25 22:33:53 +0900

    추가

--- BAEKJOON_NumberTheoryAndCombinatorialAnalysis.py
+++ BAEKJOON_NumberTheoryAndCombinatorialAnalysis.py
@@ -2,3 +2,26 @@ n = int(input())
 submultiples = sorted(list(map(int, input().split())))
 
 print(submultiples[0]*submultiples[-1])
+
+# 2609
+def gcd(n, m):
+    while m>0:
+        n, m = m, n%m
+    return n
+
+a, b = map(int, input().split())
+
+print(gcd(a,b))
+print(a*b//gcd(a,b))
+
+# 1943
+def gcd(n, m):
+    while m>0:
+        n, m = m, n%m
+    return n
+
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print(a*b//gcd(a,b))
