f3781fb570a584e1b737ab872e9fc92a32f1db52
신정섭 <wjdtjq26@naver.com>
2023-03-01 16:15:15 +0900

    추가

--- BAEKJOON_NumberTheoryAndCombinatorialAnalysis.py
+++ BAEKJOON_NumberTheoryAndCombinatorialAnalysis.py
@@ -25,3 +25,76 @@ t = int(input())
 for i in range(t):
     a, b = map(int, input().split())
     print(a*b//gcd(a,b))
+
+
+# 3036
+n = int(input())
+
+rings = list(map(int, input().split()))
+
+x = rings[0]
+
+def gcd(a,b):
+    while b>0:
+        a, b = b, a%b
+    return a
+
+for i in range(1, len(rings)):
+    g = gcd(x,rings[i])
+    print(f"{x//g}/{rings[i]//g}")
+
+
+# 1010
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, input().split())
+    a = 1
+    b = 1
+    for j in range(n):
+        a *= m
+        m -= 1
+    
+    for j in range(1, n+1):
+        b *= j
+
+    print(int(a/b))
+
+
+# 9375
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    dic = {}
+    for j in range(n):
+        a, b = input().split()
+        if b in dic:
+            dic[b] += 1
+        else:
+            dic[b] = 2
+    sum = 1
+    lst = list(dic.values())
+    for j in lst:
+        sum *= j
+    
+    print(sum-1)
+
+
+# 1676
+n = int(input())
+
+fac = 1
+
+for i in range(1, n+1):
+    fac *= i
+
+fac = str(fac)
+sum = 0
+for i in range(len(fac)-1, -1, -1):
+    if fac[i] == '0':
+        sum += 1
+    else:
+        break
+
+print(sum)
