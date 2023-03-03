1c25fea9833b4243a5cb6a6d903d158e3bcfbcde
신정섭 <wjdtjq26@naver.com>
2023-03-03 17:18:23 +0900

    해냈다

---
+++ BAEKJOON_BackTracking.py
@@ -0,0 +1,35 @@
+# 15649
+n, m = map(int, input().split())
+
+arr = [0]*m
+used = [0]*(n+1)
+
+def dfs(idx):
+    if idx == m:
+        print(*arr)
+    else:
+        for i in range(1, n+1):
+            if used[i] == 0:
+                used[i] = 1
+                arr[idx] = i
+                dfs(idx+1)
+                used[i] = 0
+
+dfs(0)
+
+
+# 15650
+n, m = map(int, input().split())
+e = n - m
+
+arr = [0]*m
+
+def arrInput(start, end, idx):
+    if idx == m:
+        print(*arr)
+        return
+    for i in range(start, end):
+        arr[idx] = i
+        arrInput(i+1, end+1, idx+1)
+
+arrInput(1, e+2, 0)
