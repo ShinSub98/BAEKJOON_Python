b171d82f04d4eb1e0b8e9e35cd7887ea8f905998
신정섭 <wjdtjq26@naver.com>
2023-03-12 15:26:23 +0900

    동적계획법 수정

--- BAEKJOON_Dynamic Programming.py
+++ BAEKJOON_Dynamic Programming.py
@@ -1,30 +1,13 @@
-def fibo(x):
-    global countFibo
+# 24416
+def fibo(x, lst):
     if x == 1 or x == 2:
         return 1
-    else:
-        countFibo += 1
-        return fibo(x-1)+fibo(x-2)
-
-def fibo_dp(x, lst):
-    global countFibo_dp
-    if x == 1 or x == 2:
-        return lst[x]
-    else:
-        for i in range(3, x+1):
-            countFibo_dp += 1
-            lst[i] = lst[i-1] + lst[i-2]
-        return lst[i]
-    
-countFibo = 0
-countFibo_dp = 0
-
-fibo_lst = [0]*50
-fibo_lst[1], fibo_lst[2] = 1, 1
+    lst[1], lst[2] = 1, 1
+    for i in range(3, n+1):
+        lst[i] = lst[i-1]+lst[i-2]
+    return lst[x]
 
 n = int(input())
+lst = [0]*41
 
-fibo(n)
-fibo_dp(n, fibo_lst)
-
-print(countFibo, countFibo_dp)
+print(fibo(n, lst), n-2)
